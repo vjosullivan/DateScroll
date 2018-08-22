13f1933ffc9ab1e865e5df1731d0d55b853f8312
vjosullivan <vjosullivan@hotmail.com>
2018-08-22 13:17:34 +0100

    Initial version (with 1000 (-200 to +800) day scroll).

---
+++ DateScroll/DateCell.swift
@@ -0,0 +1,16 @@
+//
+//  DateCell.swift
+//  DateScroll
+//
+//  Created by Vincent O'Sullivan on 22/08/2018.
+//  Copyright © 2018 Vincent O'Sullivan. All rights reserved.
+//
+
+import UIKit
+
+class DateCell: UITableViewCell {
+
+      @IBOutlet weak var month: UILabel!
+      @IBOutlet weak var dayOfMonth: UILabel!
+      
+}
--- DateScroll/ViewController.swift
+++ DateScroll/ViewController.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+      private let rowOffset = 200
+      private let monthFormatter = DateFormatter()
+      private let dayFormatter = DateFormatter()
+
+      @IBOutlet weak var calendarTable: UITableView!
 
       override func viewDidLoad() {
             super.viewDidLoad()
-            // Do any additional setup after loading the view, typically from a nib.
+
+            monthFormatter.dateFormat = "MMMM"
+            dayFormatter.dateFormat = "d"
+
+            calendarTable.delegate = self
+            calendarTable.dataSource = self
+
+            let indexPath = IndexPath(row: rowOffset, section: 0)
+            calendarTable.scrollToRow(at: indexPath, at: UITableView.ScrollPosition.top, animated: false)
+      }
+
+      func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 1000
+      }
+
+      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let dateCell = calendarTable.dequeueReusableCell(withIdentifier: "DateCell", for: indexPath) as! DateCell
+            let date = dateForRow(indexPath.row)
+            dateCell.month.text = date.month
+            dateCell.dayOfMonth.text = date.day
+            return dateCell
       }
 
 
+      /// Returns a tuple containg the date components for the given row.
+      /// Row #rowOffset corresponds to today's date.  Each row above is a day earlier and each row below is a day later.
+      ///
+      private func dateForRow(_ row: Int) -> (month: String, day: String) {
+            // Row #rowOffset contains today's date.
+            let rowDate = Date().addingTimeInterval(TimeInterval((row - rowOffset) * 86400)) + Double(row)
+
+            return (month: monthFormatter.string(from: rowDate), day: dayFormatter.string(from: rowDate))
+      }
 }
 
